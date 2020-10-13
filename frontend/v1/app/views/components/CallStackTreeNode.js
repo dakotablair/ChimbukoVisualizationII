14070959b1d2e83af28d4b4f42d30b45bf0989ad
Wei Xu <xuw@bnl.gov>
2020-10-13 18:38:10 -0400

    refine callstacktree

--- frontend/v1/app/views/components/CallStackTreeNode.js
+++ frontend/v1/app/views/components/CallStackTreeNode.js
@@ -64,7 +64,7 @@ class TreeNode extends React.Component
 
         // todo: smartly determine tooltip position... how??
         // - depends on mouse cursor position.
-        let tooltip_w = Math.max(parseFuncName(d.func).length*3, 156);  // assume 3 is character width
+        let tooltip_w = Math.max(parseFuncName(d.func).length*5, 156);  // assume 5 is character width
         let tooltip_h = 90;
         let tooltip_offset_y = 0;
         let tooltip_offset_x = 0;
