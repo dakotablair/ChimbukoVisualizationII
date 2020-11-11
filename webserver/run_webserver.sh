cbe56c1e6a27187d0e4e9bce878addfb4b3109cf
Wei Xu <xuw@bnl.gov>
2020-11-11 15:49:10 -0500

    test createdb

--- webserver/run_webserver.sh
+++ webserver/run_webserver.sh
@@ -27,7 +27,7 @@ python3 manager.py celery --loglevel=info &
 sleep 10
 
 echo "run db ..."
-python3 manager.py creatdb
+python3 manager.py createdb
 
 echo "run webserver ..."
 python3 manager.py runserver --host 0.0.0.0 --port 5002 --no-debug
