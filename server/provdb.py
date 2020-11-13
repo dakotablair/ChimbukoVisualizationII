78a19880d7afa306713e8d50d009422e602ab1f6
Wei Xu <xuw@bnl.gov>
2020-11-13 15:55:21 -0500

    debug pysonata

--- server/provdb.py
+++ server/provdb.py
@@ -36,6 +36,7 @@ class ProvDB():
         # if self.pdb_databases:
         #     for database, name in zip(self.pdb_databases, self.pdb_names):
         #         self.pdb_admin.destroy_database(self.pdb_address, 0, name)
+        print("ProvDB cleans up {} engine".format(self.pdb_address))
         if self.pdb_collections:
             for col in self.pdb_collections:
                 del col
