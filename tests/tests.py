3944bf3c586f74259c36ccb8012a4a54196d194f
Wei Xu <xuw@bnl.gov>
2020-11-12 20:23:36 -0500

    debug unittest

--- tests/tests.py
+++ tests/tests.py
@@ -201,7 +201,7 @@ class ServerTests(unittest.TestCase):
         print("......", self.app)
         r, s, h = self.post('/api/anomalydata', payload)
         self.assertEqual(s, 202)
-        time.sleep(1)
+        time.sleep(5)
 
         payload = payload['anomaly_stats']
         # check anomaly statistics
