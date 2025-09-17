024c1df12d034249621e34c12caa12a88cbdf0fc
Dakota Blair <dakota@tensen.net>
2025-09-17 17:03:11 -0400

    Update e2e-test.sh

--- docker/e2e-test.sh
+++ docker/e2e-test.sh
@@ -33,7 +33,7 @@ function hold_for_redis () {
     echo wait for redis-cli: hold on 1 sec
     sleep 1
   done
-  OUTPUT="";  
+  OUTPUT="----";  
   while [["$OUTPUT" != "PONG" ]]; do
     OUTPUT=`$REDIS_CLI PING`;
     echo wait for redis: hold on 1 sec
