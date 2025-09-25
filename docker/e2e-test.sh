59120d36939ae97dce844281259e1851d7061c5e
Dakota Blair <dakota@tensen.net>
2025-09-25 16:18:24 -0400

    Update e2e-test.sh

--- docker/e2e-test.sh
+++ docker/e2e-test.sh
@@ -59,6 +59,15 @@ killall -r '.*redis.*'
 
 popd
 
+# Install ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+    | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+      && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
+    | sudo tee /etc/apt/sources.list.d/ngrok.list \
+      && sudo apt update \
+      && sudo apt install -y ngrok
+ngrok config add-authtoken $NGROK_TOKEN
+
 RUN_SCRIPT="./run.sh"
 sed -i "s/^cycles=200/cycles=10/" $RUN_SCRIPT
 sed -i "s/mpirun/mpirun --oversubscribe/" $RUN_SCRIPT
